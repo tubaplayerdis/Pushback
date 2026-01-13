a056403493b4812bb6f69768e3270fadde14c005
tubaplayerdis <70219635+tubaplayerdis@users.noreply.github.com>
2026-01-12 22:22:20 -0600

    do sawp auton no dsr

--- src/autons/sawp_autons.cpp
+++ src/autons/sawp_autons.cpp
@@ -16,10 +16,10 @@ namespace coords
     namespace sawp
     {
         pos starting_location(-48.5, -14.5, 180);
-        pos match_loader_neg_neg(-57, -48.5, 90);
-        pos block_blip_neg_neg(-25.5, -20.5, 125);
-        pos long_goal_neg_neg(-29, -46.5, 90);
-        pos block_blip_neg_pos(-22.5, 20, 0);
+        pos match_loader_neg_neg(-57, -48.75, 90);
+        pos long_goal_neg_neg(-29.5, -49, 90);
+        pos block_blip_neg_pos(-20, 20, 0);
+        pos block_blip_neg_neg(-21, -22.5, 30);
     }
 }
 
@@ -27,7 +27,7 @@ void sawp_auton(bool b_dsr)
 {
     constexpr auto FULL_POWER = 127;
     constexpr auto NO_POWER = 0;
-    constexpr auto MATCH_LOADER = -50;
+    constexpr auto MATCH_LOADER = -45;
     constexpr auto LONG_GOAL = 10;
     constexpr auto EXHAUST_INDEX = -0.2 * FULL_POWER;
     constexpr auto EXHAUST_SCORE_LOW = -0.75 * FULL_POWER;
@@ -65,17 +65,13 @@ void sawp_auton(bool b_dsr)
 
     {
         conv->match_loader.toggle();
-        chassis->moveToPoint(-45, -41, 700, {.forwards = false, .minSpeed = 30}, false);
+        chassis->moveToPoint(-45, -41.5, 700, {.forwards = false, .minSpeed = 30}, false);
         chassis->swingToHeading(90, lemlib::DriveSide::LEFT, 700, {}, false);
     }
 
     {
-        dt->l_chassis.reset_location_force(NEG_NEG);
-    }
-
-    {
         chassis->tank(MATCH_LOADER, MATCH_LOADER, true);
-        pros::Task::delay(1100);
+        pros::Task::delay(1000);
     }
 
     {
@@ -88,38 +84,33 @@ void sawp_auton(bool b_dsr)
     {
         (void)conv->exhaust.move(EXHAUST_INDEX);
         conv->match_loader.toggle();
-        chassis->swingToHeading(35, lemlib::DriveSide::LEFT, 720, {.direction = lemlib::AngularDirection::CW_CLOCKWISE}, false);
-        chassis->moveToPoint(MPOS(coords::sawp::block_blip_neg_pos), 2000, {.forwards = false, .maxSpeed = 65}, false);
+        chassis->tank(MATCH_LOADER, MATCH_LOADER, true);//Not match loader just moving backwards
+        pros::Task::delay(300);
+        chassis->swingToHeading(20, lemlib::DriveSide::LEFT, 500, {.direction = lemlib::AngularDirection::CW_CLOCKWISE}, false);
+        chassis->moveToPoint(MPOS(coords::sawp::block_blip_neg_neg), 500, {.forwards = false, .minSpeed = 30}, false);
+        chassis->moveToPoint(MPOS(coords::sawp::block_blip_neg_pos), 1500, {.forwards = false, .maxSpeed = 75}, false);
         conv->match_loader.toggle();
     }
 
     {
         chassis->turnToHeading(110, 400, {}, false);
-        chassis->moveToPose(-9, 12, 130, 600, {}, false);
-        (void)conv->exhaust.move(0.5 * FULL_POWER);
-        pros::Task::delay(300);
+        chassis->moveToPose(-8, 12, 130, 700, {}, false);
+        (void)conv->exhaust.move(0.4 * FULL_POWER);
+        pros::Task::delay(350);
         (void)conv->exhaust.move(EXHAUST_INDEX);
     }
 
     {
-        chassis->moveToPoint(-45, 46, 1000, {.forwards = false, .minSpeed = 30}, false);
+        chassis->moveToPoint(-45, 49, 2000, {.forwards = false, .maxSpeed = 70, .minSpeed = 20}, false);
         chassis->swingToHeading(90, lemlib::DriveSide::RIGHT, 500, {}, false);
         chassis->tank(MATCH_LOADER, MATCH_LOADER, true);
-        pros::Task::delay(1100);
+        pros::Task::delay(1000);
     }
 
     {
-        chassis->moveToPoint(-29, 47, 1000, {.minSpeed = 30}, false);
+        chassis->moveToPoint(-29, 51, 1000, {.minSpeed = 30}, false);
         chassis->tank(70, 70, true);
         (void)conv->exhaust.move(FULL_POWER);
-        pros::Task::delay(1600);
-        (void)conv->exhaust.move(EXHAUST_INDEX);
-    }
-
-    {
-        chassis->tank(-50,-50, true);
-        pros::Task::delay(400);
-        chassis->tank(80, 80, true);
     }
 }
 
